6c847d668efd5a0af63f06c8fb85dd3191883a9e
Aleksey Vasilenko <vasilenko.aleksey.v@gmail.com>
2022-01-14 16:44:38 +0200

    Initial commit

---
+++ src/main/java/com/practice/spring/dao/EmployeeDAO.java
@@ -0,0 +1,7 @@
+package com.practice.spring.dao;
+import com.practice.spring.entity.Employee;
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
